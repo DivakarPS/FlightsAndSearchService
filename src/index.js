2f8d7ff0654e88b505f7acf0b5ac5ff1354644f0
Divakar <divaakaar.saakthi@gmail.com>
2023-01-28 23:00:32 +0530

    Added unique constraint and started sity repository

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
-
 const {PORT} = require('./config/serverConfig');
+const bodyParser = require("body-parser");
+
 
 const setupAndStartServer = async () =>{
 
     const app = express();
-    app.listen(PORT,()=>{
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.listen(PORT,async ()=>{
         console.log(`Server started at ${PORT}`);
     });
 }
